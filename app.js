49d26b265c972d635b1fca566fb56f0c4e230e8a
Your Name <you@example.com>
2020-08-04 18:12:11 +0400

    error

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  res.render('error,ejs')
 });
 
 // error handler
@@ -35,7 +35,9 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error',{
+    title:'Not Found!'
+  });
 });
 
 module.exports = app;
