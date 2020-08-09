774a13c658a538a20378924ee15ea9cd379eb097
Your Name <you@example.com>
2020-08-09 16:01:02 +0400

    backend router

--- app.js
+++ app.js
@@ -3,40 +3,47 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const app = express();
+
+const backendRout = require('./routes/backendRouter');
 const infoRout = require('./routes/infoRouter');
 const tourRout = require('./routes/tourRouter');
 const feedbackRout = require('./routes/feedbackRouter');
-const app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/backend',backendRout)
 app.use('/tourism', tourRout);
 app.use('/feedback', feedbackRout);
 app.use('/information', infoRout);
-app.use('/', function(req, res) {
-  res.render('frontend/index', { title: 'home page' });
+app.get('/', function(req, res) {
+  res.render('frontend/index', { title: 'Discover Aragatsotn' });
 });
 
 // catch 404 and forward to error handler
 app.use(function(req, res) {
-  res.render('error.ejs')
+  res.render('error.ejs',{
+    title:'Error:404'
+  })
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error',{
-    title:'Not Found!'
-  });
-});
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error',{
+//     title:'Not Found!'
+//   });
+// });
 module.exports = app;
